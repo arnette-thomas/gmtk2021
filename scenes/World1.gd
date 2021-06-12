c5490f341e754b5420829c7df32f8fc67036fc4e
Augustin <baraugustin26@gmail.com>
2021-06-13 00:18:48 +0200

    Added ennemis (basic, rafale & shotgun)

--- scenes/World1.gd
+++ scenes/World1.gd
@@ -9,7 +9,10 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_node("YSort/Player").main_node = self
-
+	get_node("YSort/BasicZombie").main_node = self
+	get_node("YSort/Rafale").main_node = self
+	get_node("YSort/BouletUnique").main_node = self
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- scripts/Zombie.gd
+++ scripts/Zombie.gd
@@ -2,9 +2,33 @@ extends Enemy
 
 class_name Zombie
 
+var BasicGunClass := load("res://scripts/gun/Basic.gd")
+
+var current_gun
+var fire_timer := 0.0
+onready var gun_visu := $GunVisu
+
+var main_node
+
+
+func _ready():
+	if current_gun == null:
+		current_gun = BasicGunClass.new()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
 	if target == null: return
+	gun_visu.target_position = (target.position - position).normalized()
 	
 	var dir = position.direction_to(target.position).normalized()
 	move_and_collide(dir * move_speed * delta)
+
+func fire():
+	fire_timer = current_gun.reload_time
+#	var position_centered = position + Vector2.UP * 50
+	var position_centered = gun_visu.get_node("Sprite/bout_du_gun").global_position
+	gun_visu.fire()
+	var bullets = current_gun.generate_bullets(position_centered, position_centered.direction_to(target.position))
+	for b in bullets:
+		main_node.add_child(b)
