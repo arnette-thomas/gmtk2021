bea197aa34cfc7f139ed0d5f0584318142d197bf
arnette-thomas <thomas.arnette@outlook.fr>
2021-06-12 12:07:05 +0200

    Added gamepad support

--- scripts/Chain.gd
+++ scripts/Chain.gd
@@ -8,6 +8,8 @@ onready var line = $Line
 onready var coll = $CollisionShape2D
 onready var animation = $AnimationPlayer
 
+var is_shooting = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	line.scale.x = 0
@@ -16,10 +18,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("shoot"):
-		var shooting_dir = get_local_mouse_position().normalized()
+	if Input.is_action_just_pressed("shoot_chain") && !is_shooting:
+		var aim_dir = Vector2(Input.get_action_strength("aim_right") - Input.get_action_strength("aim_left"), Input.get_action_strength("aim_down") - Input.get_action_strength("aim_up"))
+		var shooting_dir = aim_dir if aim_dir != Vector2.ZERO else get_local_mouse_position().normalized()
 		rotation = Vector2.RIGHT.angle_to(shooting_dir)
 		animation.play("shoot")
+		is_shooting = true
 
 
 func _on_AnimationPlayer_animation_finished(anim_name):
@@ -27,3 +31,4 @@ func _on_AnimationPlayer_animation_finished(anim_name):
 		line.scale.x = 0
 		coll.scale.x = 0
 		rotation = 0
+		is_shooting = false
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -4,7 +4,7 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-const MOVE_SPEED := 300
+const MOVE_SPEED := 400
 var dir := Vector2.ZERO
 
 const CAPTURE_RANGE = 150
@@ -21,7 +21,8 @@ func _process(delta):
 	# Handle inputs
 	dir.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	dir.y = Input.get_action_strength("down") - Input.get_action_strength("up")
-	dir = dir.normalized()
+	if dir.length() > 1:
+		dir = dir.normalized()
 	
 	# Move player
 	move_and_collide(dir * MOVE_SPEED * delta)
