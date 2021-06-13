2269833b7d1286e7970aeec0ac7249ecba3761ed
arnette-thomas <thomas.arnette@outlook.fr>
2021-06-13 14:28:42 +0200

    Added cow

--- scripts/Minecraft.gd
+++ scripts/Minecraft.gd
@@ -7,13 +7,24 @@ var posrand=Vector2.ZERO
 var dir=Vector2.ZERO
 var timebfdash=5
 
+onready var anim_tree : AnimationTree = $AnimationTree
+onready var sprite : Sprite = $Sprite
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	dir = position.direction_to(posrand).normalized()
+	
+	sprite.flip_h = (dir.x > 0)
+	
+	var is_moving = false
 	if $Timer.time_left>1.5:
 		if timebfdash>0:
-			move_and_collide(dir * move_speed * delta)	
+			move_and_collide(dir * move_speed * delta)
+			is_moving = true
 	
+	if is_moving:
+		anim_tree.set("parameters/idle_state/current", 1)
+	else:
+		anim_tree.set("parameters/idle_state/current", 0)
 	
 func get_random_position():
 	var x_max=get_viewport_rect().size.x
@@ -23,12 +34,14 @@ func get_random_position():
 	return Vector2(xrand,yrand)
 	
 func dash(initdir):
+	anim_tree.set("parameters/move_state/current", 1)
 	var totaltime = 0
 	while totaltime < 0.8:
 		var delta = get_process_delta_time()
 		move_and_collide(initdir * move_speed * 3 * delta)
 		totaltime += delta
 		yield(get_tree(), "idle_frame")
+	anim_tree.set("parameters/move_state/current", 0)
 
 func _on_Timer_timeout():
 	posrand=get_random_position()
